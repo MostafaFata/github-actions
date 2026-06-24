bcf53045e66ffd297ecfceb702f344924c85b2c6
Mostafa Fata <mostafa.fata@gmail.com>
2026-06-24 15:24:59 -0700

    update mvn command

--- .github/scripts/run-maven.sh
+++ .github/scripts/run-maven.sh
@@ -1,4 +1,14 @@
 
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+echo "=== Run Profile ==="
+echo "TEST_PROFILE: ${TEST_PROFILE:-}"
+echo "EXPORT_TEMPLATE: ${EXPORT_TEMPLATE:-}"
+echo "CF_RUNMODE: ${CF_RUNMODE:-}"
+echo "POM_PROFILE: ${POM_PROFILE:-}"
+
+echo "===================="
+
 mvn -version
